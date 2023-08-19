1ffb08d7c9f95a8999781e57fe52d5b183933f66
xamanDawud <ahmeddaudking100@gmail.com>
2023-08-20 01:10:54 +0600

    text-area text show on headline

---
+++ js/index.js
@@ -0,0 +1,11 @@
+let body = document.getElementById("body");
+document.getElementById("btn-yellow").addEventListener("click", function () {
+  body.style.backgroundColor = "yellow";
+});
+document.getElementById("btn-purple").addEventListener("click", function () {
+  body.style.backgroundColor = "purple";
+});
+
+document.getElementById("btn-comment").addEventListener("click", function () {
+  console.log("Hello beta");
+});
